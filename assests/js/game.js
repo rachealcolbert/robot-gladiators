07ad4086995899a49da01ea18cbbd315cb48e1a3
Racheal Colbert <rachealcolbert16@gmail.com>
2020-10-07 20:35:45 -0700

    fight

--- assests/js/game.js
+++ assests/js/game.js
@@ -1,1 +1,16 @@
-window.alert("This is an alert! JavaScript is running!")
+// this creates a function named "fight"
+
+var playerName = window.prompt("What is your robot's name?");
+
+console.log(playerName);
+
+var playerName = "Tony the Robot";
+
+// Tony the Robot is ready for battle!
+console.log("Tony the Robot" + " is ready for battle!");
+
+// "Tony the Robot is ready for battle!"
+console.log(playerName + " is ready for battle!");
+
+// "Your robot, Tony the Robot, has won!
+console.log("Your robot, " + playerName + ", has won!");
